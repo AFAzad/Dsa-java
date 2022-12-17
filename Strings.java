0f6a365122698f3fb6761c770318529ad17df14a
Abul Fajle Azad <abulfajle405@gmail.com>
2022-12-18 00:54:00 +0530

    next commit

--- Strings.java
+++ Strings.java
@@ -1,15 +1,29 @@
 import java.util.*;
+
 public class Strings {
- public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter your name");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
 
+        String name = sc.nextLine();
 
-    String name = sc.nextLine();
+        System.out.println("Your name is " + name);
 
+        // concatenation
 
-    System.out.println("Your name is " + name);
+        String firstName = "Virat";
+        String lastName = "Kolhi";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+        System.out.println(" ");
  
-
-}    
+        // CharAt().
+        System.out.println("Using charAt to Access all Charter of name");
+        for (int i = 0; i < fullName.length(); i++) {
+            
+            System.out.println(fullName.charAt(i));
+        }
+    }
 }
